fe670c9e867d7287d6a76edf1e7f739984797f07
Ernesto Junior <ernesto.sjunior@hotmail.coms>
2023-12-14 20:30:57 -0300

    chore: config db

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
 from flask import Flask
+from ext.config import Config
+from ext.database import db
 
 app = Flask(__name__)
+
+app.config.from_object(Config)
+
+db.init_app(app)
---
+++ ext/config.py
@@ -0,0 +1,3 @@
+class Config:
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://admin:admin@localhost/pybank'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
