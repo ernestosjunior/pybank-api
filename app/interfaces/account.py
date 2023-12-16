bc8d4b427ab1797df85931421ef9a183c72e5c4b
Ernesto Junior <ernesto.sjunior@hotmail.coms>
2023-12-16 18:57:41 -0300

    feat: include fields on account schema

--- app/interfaces/account.py
+++ app/interfaces/account.py
@@ -5,5 +5,7 @@ from app.models import AccountTypeEnum
 class AccountSchema(Schema):
     id = fields.Integer()
     person_id = fields.Integer(required=True)
+    balance = fields.Float()
+    daily_withdrawal_limit = fields.Float()
     status = fields.Boolean()
     type = fields.Enum(AccountTypeEnum)
