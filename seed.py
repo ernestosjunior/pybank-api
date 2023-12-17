91f9362f3dd6aaed0fae2e1d869a644183c3d649
Ernesto Junior <ernesto.sjunior@hotmail.coms>
2023-12-17 14:05:17 -0300

    fix: validation seed script

--- seed.py
+++ seed.py
@@ -2,18 +2,28 @@ from app.models.person_model import Person
 from app.utils.hash import generate_hash
 from app import create_app
 from app.extensions import db
+from sqlalchemy import or_
 
 app = create_app()
 
 with app.app_context():
-    password_hash = generate_hash("mudar123")
-    person_created = Person(
-        name="Pessoa Inicial",
-        cpf="12345678907",
-        birth_date="1997-08-02",
-        email="pessoainicial@pybank.com",
-        password=password_hash,
-    )
+    email = "pessoainicial@pybank.com"
+    cpf = "12345678907"
 
-    db.session.add(person_created)
-    db.session.commit()
+    person = Person.query.filter(or_(Person.email == email, Person.cpf == cpf)).first()
+
+    if not person:
+        password_hash = generate_hash("mudar123")
+        person_created = Person(
+            name="Pessoa Inicial",
+            cpf=cpf,
+            birth_date="1997-08-02",
+            email=email,
+            password=password_hash,
+        )
+
+        db.session.add(person_created)
+        db.session.commit()
+        print("Seed applied.")
+    else:
+        print("There is no seed to apply.")
